f0434264ee85bd89ff480e21808431483b9f24cf
arodewitt <alicia@Alicias-MacBook-Pro.local>
2017-02-15 11:04:59 -0500

    site for IDEO app

---
+++ nova.php
@@ -0,0 +1,23 @@
+<?php include('../design/region/nav.php'); ?>
+
+<div class="title">
+				<hr />
+				</br>
+				<h2></h2>
+				<h1>PBS Nova Next</h1>
+				<h3>"Fixing the Climate" Infographic</h3>
+        <h3>Digital, various sizes</h3>
+			</div>
+			<div class="content">
+        <p>NOVA knew that they wanted an infographic to accompany an online piece about the impact that self driving cars would have on the climate. Their idea was to quantify how we could reduce global emissions, but there wasn’t much more than that at first.
+        <p>I started by organizing the list of facts that they were hoping to include. Two major categories of information emerged: large trends in renewable energy, and smaller factors that might feel like more tangible changes to someone’s lifestyle.
+        <p>I iterated back and forth with NOVA over emails and phone calls. Ultimately, the story became about showing that climate change isn’t a lost cause — there really are small changes that will help. We decided to open with more abstract representations of the big numbers, and then show the small changes with familiar iconography. I knew I was on to something when my editor opened a check-in with “you definitely got your Tufte on.” </p>
+				<img src="../design/graphics/CMS6.jpg" alt="CMS poster" width="717px" />
+        <p> </p>
+				<img src="../design/graphics/CMS-poster1.jpg" alt="CMS poster" width="717px" />
+        <p> </p>
+        <img src="../design/graphics/CMS3.jpg" alt="CMS poster" width="717px" />
+        <p> </p>
+			</div>
+
+<?php include('../design/region/gallery.php'); ?>
---
+++ region/nav.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+ 	<head>
+ 		<meta name="description" content="Illustration and Design" />
+		<title>Alicia DeWitt - Graphic Design</title>
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Source+Serif+Pro:700,400' rel='stylesheet' type='text/css'>
+		<link href="style.css" type="text/css" rel="stylesheet" />
+	</head>
+	</body>
+    <div class="wrapper">
+
+			<div class="nav">
+				<h1>a.</h1>
+        <h4>alicia dewitt design</h4>
+        <hr class="full-width">
+      <!-- </br></br></br>
+				<p>Hello IDEO! Here are some projects that I love talking about. Please connect if you want to hear or see more!</p>
+        </br></br> -->
+      </div>
